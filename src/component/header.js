bacc9d28a15bca20f8802aa22accc1fe6a145f69
garry <lugh60@gmail.com>
2022-02-07 14:47:50 +0800

    add docs

--- src/component/header.js
+++ src/component/header.js
@@ -7,13 +7,13 @@ import Metamask from "./wallet/metamask";
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    const strs = window.location.href.split('/')
-    this.state = {button_selected: [false, false, false]};
-    if (strs[strs.length - 1] === "about") {
-      this.state = {button_selected: [false, false, true]};
-    } else if (strs[strs.length - 1] === "DAO") {
-      this.state = {button_selected: [false, true, false]};
-    }
+    // const strs = window.location.href.split('/')
+    this.state = {button_selected: 0};
+    // if (strs[strs.length - 1] === "about") {
+    //   this.state = {button_selected: [false, false, true]};
+    // } else if (strs[strs.length - 1] === "DAO") {
+    //   this.state = {button_selected: [false, true, false]};
+    // }
   }
 
   scrollToAnchor = (anchorName) => {
@@ -38,49 +38,62 @@ class Header extends React.Component {
              }}
              alt=""/>
         <div className="header_frame">
-          <button className={this.state.button_selected[0] === true ? "button_selected" : "button_empty"}
+          <button className={this.state.button_selected === 1 ? "button_selected" : "button_empty"}
                   style={{cursor: 'pointer'}}
                   onMouseEnter={() => {
-                    this.setState({button_selected: [true, false, false]})
+                    this.setState({button_selected: 1})
                   }}
                   onMouseLeave={() => {
-                    this.setState({button_selected: [false, false, false]})
+                    this.setState({button_selected: 0})
                   }}
                   onClick={() => {
                     history.push('/#/product')
                     window.location.reload()
                   }}>
-            <div className={this.state.button_selected[0] === true ? "word_selected" : "word_empty"}>Product</div>
+            <div className={this.state.button_selected === 1 ? "word_selected" : "word_empty"}>Product</div>
           </button>
           <a style={{textDecoration: "none"}}
              href="https://snapshot.org/#/digifun.eth">
-            <button className={this.state.button_selected[1] === true ? "button_selected" : "button_empty"}
+            <button className={this.state.button_selected === 2 ? "button_selected" : "button_empty"}
                     style={{cursor: 'pointer'}}
                     onMouseEnter={() => {
-                      this.setState({button_selected: [false, true, false]})
+                      this.setState({button_selected: 2})
                     }}
                     onMouseLeave={() => {
-                      this.setState({button_selected: [false, false, false]})
+                      this.setState({button_selected: 0})
                     }}>
-              <div className={this.state.button_selected[1] === true ? "word_selected" : "word_empty"}>DAO</div>
+              <div className={this.state.button_selected === 2 ? "word_selected" : "word_empty"}>DAO</div>
             </button>
           </a>
-          <button className={this.state.button_selected[2] === true ? "button_selected" : "button_empty"}
+          <button className={this.state.button_selected === 3 ? "button_selected" : "button_empty"}
                   style={{cursor: 'pointer'}}
                   onMouseEnter={() => {
-                    this.setState({button_selected: [false, false, true]})
+                    this.setState({button_selected: 3})
                   }}
                   onMouseLeave={() => {
-                    this.setState({button_selected: [false, false, false]})
+                    this.setState({button_selected: 0})
                   }}
                   onClick={() => {
                     history.push('/#/about')
                     window.location.reload()
                   }}>
-            <div className={this.state.button_selected[2] === true ? "word_selected" : "word_empty"}>About</div>
+            <div className={this.state.button_selected === 3 ? "word_selected" : "word_empty"}>About</div>
           </button>
+          <a style={{textDecoration: "none"}}
+             href="https://digifundao.com/docs">
+            <button className={this.state.button_selected === 4 ? "button_selected" : "button_empty"}
+                    style={{cursor: 'pointer'}}
+                    onMouseEnter={() => {
+                      this.setState({button_selected: 4})
+                    }}
+                    onMouseLeave={() => {
+                      this.setState({button_selected: 0})
+                    }}>
+              <div className={this.state.button_selected === 4 ? "word_selected" : "word_empty"}>Docs</div>
+            </button>
+          </a>
         </div>
-        <Metamask/>
+        {/*<Metamask/>*/}
         {/*<div className="header_wallet">*/}
         {/*  <button className="button_wallet"*/}
         {/*          style={{cursor: 'pointer'}}*/}
