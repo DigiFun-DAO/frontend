ba5f49e86ee924d2cedd20a3736c642ca131c901
garry <lugh60@gmail.com>
2022-04-19 18:42:42 +0800

    add tracking

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
 import Home from './component/home/home'
 import React from "react";
+import ReactGA from 'react-ga'
 
 class App extends React.Component {
+  componentDidMount() {
+    ReactGA.initialize('G-4MJ7F3CJWQ');
+    ReactGA.set({page: window.location.pathname});
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    ReactGA.set({page: window.location.pathname});
+    ReactGA.pageview(window.location.pathname);
+  }
+
   render() {
     return (
       <div className="App">
